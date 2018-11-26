af6470bc6781c6e1f5faf34b911a6cd78cbde808
Meg Cruzen <megcruzen@gmail.com>
2018-11-26 10:32:28 -0600

    First commit

---
+++ pet.js
@@ -0,0 +1,16 @@
+/* Create an object that represents your pet.
+Name property with a string value.
+Species property with a string value.
+Nicknames property with an array value. Array contains strings.
+Age property with an integer value.
+*/
+
+const pet = {
+    name: "Juniper",
+    species: "cat",
+    nicknames: ["June", "Joob", "Joobie", "Fatty"],
+    age: 12
+};
+
+console.log(pet.name);
+console.log(pet.nicknames[2]);
